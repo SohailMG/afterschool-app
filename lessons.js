edc3425378b67c6a0b0ef0fa69338214e8ecfa0b
Sohail Gsais <Gsais.sohail9@gmail.com>
2021-11-09 11:22:44 +0000

    initial commit

---
+++ lessons.js
@@ -0,0 +1,58 @@
+const lessons = [
+  {
+    id: 1,
+    subject: "English",
+    location: "London",
+    price: 20.5,
+    space: 5,
+    image:
+      "https://www.pinclipart.com/picdir/middle/197-1977094_vector-illustration-of-school-mathematics-study-in-math.png",
+  },
+  {
+    id: 2,
+    subject: "Maths",
+    location: "Hendon",
+    price: 15.5,
+    space: 5,
+    image:
+      "https://www.pinclipart.com/picdir/middle/197-1977094_vector-illustration-of-school-mathematics-study-in-math.png",
+  },
+  {
+    id: 3,
+    subject: "Science",
+    location: "London",
+    price: 10.0,
+    space: 5,
+    image:
+      "https://www.pinclipart.com/picdir/middle/197-1977094_vector-illustration-of-school-mathematics-study-in-math.png",
+  },
+  {
+    id: 4,
+    subject: "Music",
+    location: "Hendon",
+    price: 10.0,
+    space: 5,
+    image:
+      "https://www.pinclipart.com/picdir/middle/197-1977094_vector-illustration-of-school-mathematics-study-in-math.png",
+  },
+  {
+    id: 5,
+    subject: "Art",
+    location: "Hendon",
+    price: 10.0,
+    space: 5,
+    image:
+      "https://www.pinclipart.com/picdir/middle/197-1977094_vector-illustration-of-school-mathematics-study-in-math.png",
+  },
+  {
+    id: 6,
+    subject: "Music",
+    location: "Hendon",
+    price: 10.0,
+    space: 5,
+    image:
+      "https://www.pinclipart.com/picdir/middle/197-1977094_vector-illustration-of-school-mathematics-study-in-math.png",
+  },
+];
+
+export default lessons;
